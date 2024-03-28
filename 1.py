1d88c3789b1149fc7c3c61e12306d0b5ae495e5a
1 <diany.kor.2019@mail.ru>
2024-03-28 13:15:34 +0300

    solutions\

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 countries_capitals = { "Россия": "Москва", "Япония": "Токио", "Германия": "Берлин",
-    "Великобритания": "Лондон, округ Колумбия", "Китай": "Пекин"}
+    "Великобритания": "Лондон", "Китай": "Пекин"}
 
 print("Пары ключ-значение в словаре:")
 for country, capital in countries_capitals.items():
@@ -9,6 +9,6 @@ country = "Япония"
 print(f"Столица страны '{country}': {countries_capitals.get(country)}")
 
 sorted_countries = sorted(countries_capitals.keys())
-print("\nСодержимое словаря, отсортированное по названию стран:")
+print("Содержимое словаря, отсортированное по названию стран:")
 for country in sorted_countries:
     print(country, "-", countries_capitals[country])
